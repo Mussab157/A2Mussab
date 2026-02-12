a73e5b83f0d5401b67d47bda5a908f7364bab1d4
Mussab157 <mussabintel@gmail.com>
2026-02-12 11:37:52 -0500

    introduction, first commit

---
+++ src/test/java/com/example/a2mussab/A2MussabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a2mussab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2MussabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
